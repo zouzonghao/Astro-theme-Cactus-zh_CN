f979eb130aeaebf305ed223fb58525edaad159b4
chirophy <43292230+chirophy@users.noreply.github.com>
2025-04-23 17:30:51 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
         /**
          * optional
          **/
-        short_name: "仙人掌主题",
+        short_name: "Neoz小站",
         description: siteConfig.description,
         lang: siteConfig.lang,
         icon: "public/icon.svg", // the source for generating favicon & icons
@@ -110,7 +110,7 @@ export default defineConfig({
     prefetchAll: true,
   },
     // ! 改为你的网站地址，不然社交图片无法加载
-    site: "https://demo.343700.xyz/",
+    site: "https://241126.xyz/",
     vite: {
         optimizeDeps: {
             exclude: ["@resvg/resvg-js"],
