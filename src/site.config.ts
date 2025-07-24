de636e5a54e8b011262fe4380817948926c3d774
地球观察员 <77538186+Versior@users.noreply.github.com>
2025-07-24 19:34:17 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -20,7 +20,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property, found in src/components/BaseHead.astro L:42
 	ogLocale: "zh-CN",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "贰零贰伍",
+	title: "落叶下的贰伍秋",
 };
 
 // Used to generate links in both the Header & Footer.
